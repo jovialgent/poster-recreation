4d2cf7f70a8cf759ed9a72c0baedb66b6be14d0d
George <gfrankpetersen@gmail.com>
2022-03-02 21:52:39 -0700

    metro - set up

--- src/lib/Metro.svelte
+++ src/lib/Metro.svelte
@@ -12,21 +12,26 @@
   @use "sass:math";
   @import "../scss/poster.scss";
 
-  :host {
-    --metro-background: black;
+  @mixin metroContentWrapper() {
+    @include containerWrapper();
+    padding: 0.5rem;
+    border-radius: 0.25rem;
   }
 
   .poster {
+    --metro-background: black;
+    --metro-background-border: #e2e3e0;
     @include poster();
 
     &__container {
       &--wrapper {
-        @include containerWrapper();
+        @include metroContentWrapper();
+        background: var(--metro-background-border);
       }
-      *--content {
+      &--content {
         background: var(--metro-background);
-        width:100%;
-        height:100%;
+        width: 100%;
+        height: 100%;
       }
     }
   }
