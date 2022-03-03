f36d59bac24a2751e7db774ccbfa8c37ba0dd3ca
George <gfrankpetersen@gmail.com>
2022-03-02 21:31:20 -0700

    metro - nit

---
+++ src/lib/Metro.svelte
@@ -0,0 +1,22 @@
+<script></script>
+
+<div class="poster">
+  <section class="poster__container--wrapper">
+    
+  </section>
+</div>
+
+<style lang="scss">
+  @use "sass:math";
+  @import "../scss/poster.scss";
+
+  .poster {
+    @include poster();
+    
+    &__container {
+      &--wrapper {
+        @include containerWrapper();
+      }
+    }
+  }
+</style>
