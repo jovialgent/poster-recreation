63447822242e57e1bfa5322ad0eb9d6cc677e24d
George <gfrankpetersen@gmail.com>
2022-03-02 20:31:53 -0700

    main - trying to get pages to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/poster-recreation/",
   build: {
-    outDir: "docs"
+    outDir: "dist",
   },
 });
