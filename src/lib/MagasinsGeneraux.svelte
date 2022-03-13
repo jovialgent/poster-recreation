89c67625518dbd48fa3334701e7697f385947d44
George <gfrankpetersen@gmail.com>
2022-03-13 14:10:19 -0600

    magasins-generaux - Setting up poster

--- src/lib/MagasinsGeneraux.svelte
+++ src/lib/MagasinsGeneraux.svelte
@@ -1,21 +1,19 @@
 <script>
-// https://www.behance.net/gallery/97124351/Poster-Collection/modules/560843111
-
+  // https://www.behance.net/gallery/97124351/Poster-Collection/modules/560843111
 </script>
 
 <div class="poster">
-  <section class="poster__container--wrapper">
-    
-  </section>
+  <section class="poster__container--wrapper" />
 </div>
 
 <style lang="scss">
   @use "sass:math";
   @import "../scss/poster.scss";
+  @import url("https://use.typekit.net/pwc5ubl.css");
 
   .poster {
     @include poster();
-    
+
     &__container {
       &--wrapper {
         @include containerWrapper();
